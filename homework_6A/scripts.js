e6fcbc4d9dfd9b79025582273c09c7adbab2683a
Emily Feng <manqingf@andrew.cmu.edu>
2021-04-01 11:11:00 +0800

    finished 6A

--- homework_6A/scripts.js
+++ homework_6A/scripts.js
@@ -1,115 +1,88 @@
-function cat_harness(color, size, qty) {
-    this.name = "Cat Harness";
-    this.price = 26.85;
-    this.color = color;
-    this.size = size;
-    this.qty = qty;
-    this.image_alt = "cat harness";
-    this.image = "images/cat_harness.png";
+var num = 0;
+
+function clearAll() {
+    localStorage.clear();
 }
-
-function dog_harness(color, size, qty) {
-    this.name = "Dog Harness";
-    this.price = 29.85;
-    this.color = color;
-    this.size = size;
-    this.qty = qty;
-    this.image_alt = "dog harness";
-    this.image = "images/dog_harness.png";
-}
-
-function food_storage(color, size, qty) {
-    this.name = "Food Storage Attachment to Harness";
-    this.price = 124.95;
-    this.color = color;
-    this.size = size;
-    this.qty = qty;
-    this.image_alt = "Food Storage Attachment to Harness";
-    this.image = "images/foodStorage.png";
-}
-
-function water_storage(color, size, qty) {
-    this.name = "Water Storage Attachment to Harness";
-    this.price = 26.99;
-    this.color = color;
-    this.size = size;
-    this.qty = qty;
-    this.image_alt = "Water Storage Attachment to Harness";
-    this.image = "images/waterStorage.png";
-}
-
-function collar(color, size, qty) {
-    this.name = "Dog/Cat GPS Tracker Collar";
-    this.price = 31.49;
-    this.color = color;
-    this.size = size;
-    this.qty = qty;
-    this.image_alt = "GPS Tracker Collar";
-    this.image = "images/collar.png";
-}
-
-function backpack(color, size, qty) {
-    this.name = "Cat Backpack";
-    this.price = 127.65;
-    this.color = color;
-    this.size = size;
-    this.qty = qty;
-    this.image_alt = "cat backpack";
-    this.image = "images/backpack.png";
-}
-
-var products = [ new cat_harness(), new dog_harness(), new food_storage(), new water_storage(), new collar(), new backpack()];
-
-var colors = ["Strawberry", "Blackberry", "Crazyberry", "Fire Orange"];
-
-var sizes = ["Tiny", "Small", "Medium", "Large"];
-
-function generateItem(option, color, size, qty) {
-    let item = products[option];
-    if (item instanceof cat_harness)
-    {
-        return new cat_harness(color, size, qty);
-    }
-    else if (item instanceof dog_harness)
-    {
-        return new dog_harness(color, size, qty);
-    }
-    else if (item instanceof food_storage)
-    {
-        return new food_storage(color, size, qty);
-    }
-    else if (item instanceof water_storage)
-    {
-        return new water_storage(color, size, qty);
-    }
-    else if (item instanceof collar)
-    {
-        return new collar(color, size, qty);
-    }
-    else if (item instanceof backpack)
-    {
-        return new backpack(color, size, qty);
-    }
-}
-
-var addedItems = []
 
 function addListItem() {
-    let list = document.getElementById("item-list");
+    var qty = document.getElementById("qty").value;
+    localStorage.setItem("qty", qty);
     
-    let inputOption = 5;
-    let inputColor = document.getElementById("color1");
-    let inputSize = document.getElementById("size1");
-    let inputQty = document.getElementById("qty");
-    let itemInput = generateItem(inputOption, inputColor, inputSize, inputQty);
+   localStorage.setItem("num", localStorage.getItem("num")+1);
 
-    addListItems.push(itemInput);
+    alert("Added to Cart");
+}
+
+function color1(){
+    var colorInput = document.getElementById("colorInput");
+    colorInput.innerHTML = "Color: Strawberry";
+    localStorage.setItem("color", "Strawberry");
+}
+
+function color2(){
+    var colorInput = document.getElementById("colorInput");
+    colorInput.innerHTML = "Color: Blackberry";
+    localStorage.setItem("color", "Blackberry");
+}
+
+function color3(){
+    var colorInput = document.getElementById("colorInput");
+    colorInput.innerHTML = "Color: Crazyberry";
+    localStorage.setItem("color", "Crazyberry");
+}
+
+function color4(){
+    var colorInput = document.getElementById("colorInput");
+    colorInput.innerHTML = "Color: Fire Orange";
+    localStorage.setItem("color", "Fire Orange");
+}
+
+function setSize1() {
+    localStorage.setItem("size", "Tiny");
+}
+
+function setSize2() {
+    localStorage.setItem("size", "Small");
+}
+
+function setSize3() {
+    localStorage.setItem("size", "Medium");
+}
+
+function setSize4() {
+    localStorage.setItem("size", "Large");
 }
 
 function onLoad() {
-    let title = document.getElementById("title");
-    console.log(addedItems);
-    let numItems = addedItems.length;
-    title.innerHTML = numItems + " items";
+    // header: count items in the cart
+    var header = document.getElementById("header");
+    header.innerHTML = localStorage.getItem("num").length + " Items";
 
+    // list
+    let list = document.getElementById("list");
+    let newItem = document.createElement("li");
+    newItem.id = "item";
+
+    let itemImage = document.createElement("img");
+    itemImage.src = "images/backpack1.png";
+    itemImage.alt = "cat backpack";
+    itemImage.id = "item-image";
+    let itemTitle = document.createElement("h4");
+    itemTitle.appendChild(document.createTextNode("Cat Backpack"));
+    let itemPrice = document.createElement("h4");
+    itemPrice.appendChild(document.createTextNode("$127.65"));
+    let itemColor = document.createElement("h4");
+    itemColor.appendChild(document.createTextNode("Color: " + localStorage.getItem("color")));
+    let itemSize = document.createElement("h4");
+    itemSize.appendChild(document.createTextNode("Size: " + localStorage.getItem("size")));
+    let itemQty = document.createElement("h4");
+    itemQty.appendChild(document.createTextNode("Quantity: " + localStorage.getItem("qty")));
+
+    newItem.appendChild(itemImage);
+    newItem.appendChild(itemTitle);
+    newItem.appendChild(itemPrice);
+    newItem.appendChild(itemColor);
+    newItem.appendChild(itemSize);
+    newItem.appendChild(itemQty);
+    list.appendChild(newItem);
 }
