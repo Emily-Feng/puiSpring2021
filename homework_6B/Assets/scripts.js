e9f9798cd4d49aca0000bd525c2e22048564c89d
Emily Feng <manqingf@andrew.cmu.edu>
2021-04-05 15:48:27 +0800

    starting homework 6B

---
+++ homework_6B/Assets/scripts.js
@@ -0,0 +1,88 @@
+var num = 0;
+
+function clearAll() {
+    localStorage.clear();
+}
+
+function addListItem() {
+    var qty = document.getElementById("qty").value;
+    localStorage.setItem("qty", qty);
+    
+   localStorage.setItem("num", localStorage.getItem("num")+1);
+
+    alert("Added to Cart");
+}
+
+function color1(){
+    var colorInput = document.getElementById("colorInput");
+    colorInput.innerHTML = "Color: Strawberry";
+    localStorage.setItem("color", "Strawberry");
+}
+
+function color2(){
+    var colorInput = document.getElementById("colorInput");
+    colorInput.innerHTML = "Color: Blackberry";
+    localStorage.setItem("color", "Blackberry");
+}
+
+function color3(){
+    var colorInput = document.getElementById("colorInput");
+    colorInput.innerHTML = "Color: Crazyberry";
+    localStorage.setItem("color", "Crazyberry");
+}
+
+function color4(){
+    var colorInput = document.getElementById("colorInput");
+    colorInput.innerHTML = "Color: Fire Orange";
+    localStorage.setItem("color", "Fire Orange");
+}
+
+function setSize1() {
+    localStorage.setItem("size", "Tiny");
+}
+
+function setSize2() {
+    localStorage.setItem("size", "Small");
+}
+
+function setSize3() {
+    localStorage.setItem("size", "Medium");
+}
+
+function setSize4() {
+    localStorage.setItem("size", "Large");
+}
+
+function onLoad() {
+    // header: count items in the cart
+    var header = document.getElementById("header");
+    header.innerHTML = localStorage.getItem("num").length + " Items";
+
+    // list
+    let list = document.getElementById("list");
+    let newItem = document.createElement("li");
+    newItem.id = "item";
+
+    let itemImage = document.createElement("img");
+    itemImage.src = "Assets/images/backpack1.png";
+    itemImage.alt = "cat backpack";
+    itemImage.id = "item-image";
+    let itemTitle = document.createElement("h4");
+    itemTitle.appendChild(document.createTextNode("Cat Backpack"));
+    let itemPrice = document.createElement("h4");
+    itemPrice.appendChild(document.createTextNode("$127.65"));
+    let itemColor = document.createElement("h4");
+    itemColor.appendChild(document.createTextNode("Color: " + localStorage.getItem("color")));
+    let itemSize = document.createElement("h4");
+    itemSize.appendChild(document.createTextNode("Size: " + localStorage.getItem("size")));
+    let itemQty = document.createElement("h4");
+    itemQty.appendChild(document.createTextNode("Quantity: " + localStorage.getItem("qty")));
+
+    newItem.appendChild(itemImage);
+    newItem.appendChild(itemTitle);
+    newItem.appendChild(itemPrice);
+    newItem.appendChild(itemColor);
+    newItem.appendChild(itemSize);
+    newItem.appendChild(itemQty);
+    list.appendChild(newItem);
+}
---
+++ homework_6B/README.txt
@@ -0,0 +1,29 @@
+This is for Programming Usable Interfaces 05430 Spring 2021 Assignment 6 using starter code from homework 5.
+
+Assignment 6A:
+The goal of 6A is to build out the HTML+CSS of a shopping cart, add some JS to the Product 
+Detail page to support different customization options, and to add some JS to show feedback for 
+items being added to the shopping cart.
+
+1. Create a NEW page.
+    - The page should be a Shopping cart page where users can view items in their
+        cart. Include the following prototypes: a low fidelity prototype, a high fidelity 
+        prototype, and an implementation in HTML + CSS. Modify your existing web 
+        store files as needed.
+    - If you had a shopping cart page already, choose a new type of page to 
+        implement (Special Offers, Order Tracking, etc.). You need to design high 
+        fidelity prototypes, and an implementation in HTML+CSS (no JavaScript). 
+        This is to ensure everyone does the same amount of work for this assignment. 
+        The remainder of the assignment that uses Javascript to add functionality will 
+        be implemented on the shopping cart page.
+
+
+2. Create NEW features for your site.
+    - The user can modify options on the Product Detail page (the size, quantity, etc.).
+    - The Product Detail page should change according to the user’s selection. For
+        example, if a user selected a Tiny Fire Orange Cat Harness, the page should 
+        update with the details of that product (i.e. update size, color and image). 
+        This should NOT be a new HTML file. Use JavaScript to implement this functionality.
+    - The user can add products to their shopping cart. There should be some visual 
+        indication to the user of how many items they have in their shopping cart for each 
+        page showing the shopping cart link.
